6b515b0a329ac452d25ab9caa894e38801411f32
liugang <1627035841@qq.com>
2019-02-16 22:51:59 +0800

    spring-threadpool 线程池的几种实现方式初次提交

---
+++ spring-threadpool/src/main/java/com/cn/lg/springthreadpool/SpringThreadpoolApplication.java
@@ -0,0 +1,14 @@
+package com.cn.lg.springthreadpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThreadpoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThreadpoolApplication.class, args);
+    }
+
+}
+
