da433d0b031faa9734b7720c242b8de246f66eeb
liugang <1627035841@qq.com>
2019-02-17 15:16:41 +0800

    spring-threadpool 单线程的3种实现方式

---
+++ spring-threadpool/src/main/java/com/cn/lg/springthreadpool/a/thread/SingleThread.java
@@ -0,0 +1,81 @@
+package com.cn.lg.springthreadpool.a.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @Auther: 刘钢
+ * @Date: 2019/2/16 23:23
+ * @Description: 单线程的使用
+ */
+public class SingleThread {
+
+    public static void main(String[] args) throws Exception{
+        threadStart();
+    }
+
+
+    /**
+     * 新建线程
+     * 1.继承Thread
+     * 2.实现Runnable接口
+     * 3.实现Callable接口通过FutureTask包装器来创建Thread线程
+     */
+    public static void threadStart() throws InterruptedException {
+        // 1.继承Thread
+        threadExtend threadExtend = new threadExtend();
+        threadExtend.start();
+
+        Thread.sleep(500L);
+        System.out.println("------------");
+
+        // 2.实现Runnable接口
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + "线程启动： 实现Runnalbe接口")).start();
+
+        Thread.sleep(500L);
+        System.out.println("------------");
+
+        // 3.实现Callable接口通过FutureTask包装器来创建Thread线程
+        Callable<Integer> oneCallable = new SomeCallable<>();
+        ((SomeCallable<Integer>) oneCallable).setResult(10);
+        //由Callable<Integer>创建一个FutureTask<Integer>对象：
+        FutureTask<Integer> oneTask = new FutureTask<>(oneCallable);
+        // FutureTask<Integer>是一个包装器，它通过接受Callable<Integer>来创建，它同时实现了Future和Runnable接口。
+        // 由FutureTask<Integer>创建一个Thread对象：
+        Thread oneThread = new Thread(oneTask);
+        oneThread.start();
+
+        Thread.sleep(500L);
+
+        // 或者：
+        int result = 10;
+        new Thread(new FutureTask<>(()->{
+            System.out.println(Thread.currentThread().getName() + "线程启动： 实现Callable接口 Lambda表达式");
+            return result;
+        })).start();
+
+    }
+
+}
+
+class threadExtend extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "线程启动： 继承Thread类");
+    }
+
+}
+
+class SomeCallable<Integer> implements Callable<Integer> {
+    private Integer result;
+    @Override
+    public Integer call() throws Exception{
+        System.out.println(Thread.currentThread().getName() + "线程启动： 实现Callable接口");
+        return result;
+    }
+
+    public void setResult(Integer result) {
+        this.result = result;
+    }
+}
